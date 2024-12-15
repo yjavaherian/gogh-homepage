cfc68ba05a0f877217e18e36534462ee6f514b65
Yousef <yousefjavaherian@gmail.com>
2024-12-15 18:18:39 +0330

    theming the home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,110 +1,86 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
+import { useState } from "react";
+import vangophSignature from "./assets/goph.svg";
 import oldThemes from "./assets/themes.json" with { type: "json" };
 import Terminal, { Theme } from "./Terminal.tsx";
+import { convertTheme, lightOrDark } from "./utils.ts";
+import { Quote } from "lucide-react";
 
-interface oldTheme {
-    name: string  
-    color_01: string
-    color_02: string
-    color_03: string
-    color_04: string
-    color_05: string
-    color_06: string
-    color_07: string
-    color_08: string
-    color_09: string
-    color_10: string
-    color_11: string
-    color_12: string
-    color_13: string
-    color_14: string
-    color_15: string
-    color_16: string
-    background: string 
-    foreground: string 
-    cursor: string
-}
-
-function convertTheme(theme:oldTheme){
-  return {
-    name:theme.name,
-    background:theme.background,
-    foreground: theme.foreground,
-    cursor: theme.cursor,
-    colors:[
-      theme.color_01,
-      theme.color_02,
-      theme.color_03,
-      theme.color_04,
-      theme.color_05,
-      theme.color_06,
-      theme.color_07,
-      theme.color_08,
-      theme.color_09,
-      theme.color_10,
-      theme.color_11,
-      theme.color_12,
-      theme.color_13,
-      theme.color_14,
-      theme.color_15,
-      theme.color_16
-    ]
-  }
-}
 
 function App() {
-  let themes:Theme[]  = oldThemes.map(convertTheme);
-  themes = themes.filter(theme => theme.name == 'Espresso')
+  const [filteredBackgrounds,setFilteredBackgrounds] = useState<string[]>(['light','dark']);
+
+  const  themes:Theme[]  = oldThemes.map(convertTheme).filter(theme =>  filteredBackgrounds.includes(lightOrDark(theme.background)));
+  // themes = themes.filter(theme => theme.name == 'Espresso')
   return (
-    <body>
-      <h1>Goph</h1>
-      <section>
-        <h2>Color scheme for your terminal</h2>
-        <p>
+    <main className="pt-20 flex flex-col place-items-center font-sans text-neutral-700">
+      <header className="flex flex-col place-items-center" >
+        <img className="w-96" src={vangophSignature} alt="goph logo" />
+        <blockquote className="mt-6 italic text-2xl ">
+         
+        There is no <span className="text-blue-400">blue</span> without <span className="text-yellow-400">yellow</span> and without <span className="text-orange-400">orange</span>.
+         {/* <Quote  className="ml-1 w-4 h-4 inline"/> */}
+         <br />
+        <a href="https://en.wikipedia.org/wiki/Vincent_van_Gogh" className="text-amber-800 hover:text-amber-900 hover:underline">
+        - Vincent Van Gogh
+        </a>
+        </blockquote>
+        
+      </header>
+      <section className="mt-20 w-2/3">
+        <h2 className="font-bold text-4xl">Color scheme for your terminal</h2>
+        <p className="mt-4 text-2xl ">
           Gogh is a collection of color schemes for various terminal emulators.
           These schemes are designed to make your terminal more visually
           appealing and improve your productivity by providing a better contrast
           and color differentiation.
         </p>
       </section>
-      <section>
-        <h2>Supported Terminals</h2>
-        <ul>
-          <li>Alacritty</li>
-          <li>Gnome Terminal</li>
-          <li>Konsole</li>
-          <li>Cygwin </li>
-          <li>Foot</li>
-          <li>Guake</li>
-          <li>iTerm</li>
-          <li>Kitty</li>
-          <li>Mate</li>
-          <li>Mintty</li>
-          <li>Pantheon / Elementary</li>
-          <li>Tilix</li>
-          <li>XFCE4</li>
-          <li>Terminator</li>
+
+       <section className="mt-8 w-2/3">
+        <h2 className="font-bold text-4xl" >Supported Terminals</h2>
+        <ul className="mt-4 flex flex-row gap-2 flex-wrap ">
+          <button  className="rounded-xl bg-neutral-700 text-neutral-100 hover:text-neutral-200 hover:border-e-neutral-800  py-1 px-2 text-lg "  ><a target="_blank" href="https://github.com/alacritty/alacritty">Alacritty</a></button>
+          <button  className="rounded-xl bg-neutral-700 text-neutral-100 hover:text-neutral-200 hover:border-e-neutral-800  py-1 px-2 text-lg "  ><a target="_blank" href="https://www.cygwin.com/">Cygwin</a></button>
+          <button  className="rounded-xl bg-neutral-700 text-neutral-100 hover:text-neutral-200 hover:border-e-neutral-800  py-1 px-2 text-lg "  ><a target="_blank" href="https://codeberg.org/dnkl/foot">Foot</a></button>
+          <button  className="rounded-xl bg-neutral-700 text-neutral-100 hover:text-neutral-200 hover:border-e-neutral-800  py-1 px-2 text-lg "  ><a target="_blank" href="https://help.gnome.org/users/gnome-terminal/stable/">Gnome Terminal</a></button>
+          <button  className="rounded-xl bg-neutral-700 text-neutral-100 hover:text-neutral-200 hover:border-e-neutral-800  py-1 px-2 text-lg "  ><a target="_blank" href="http://guake-project.org/">Guake</a></button>
+          <button  className="rounded-xl bg-neutral-700 text-neutral-100 hover:text-neutral-200 hover:border-e-neutral-800  py-1 px-2 text-lg "  ><a target="_blank" href="https://iterm2.com/">iTerm</a></button>
+          <button  className="rounded-xl bg-neutral-700 text-neutral-100 hover:text-neutral-200 hover:border-e-neutral-800  py-1 px-2 text-lg "  ><a target="_blank" href="https://sw.kovidgoyal.net/kitty/">Kitty</a></button>
+          <button  className="rounded-xl bg-neutral-700 text-neutral-100 hover:text-neutral-200 hover:border-e-neutral-800  py-1 px-2 text-lg "  ><a target="_blank" href="https://konsole.kde.org/">Konsole</a></button>
+          <button  className="rounded-xl bg-neutral-700 text-neutral-100 hover:text-neutral-200 hover:border-e-neutral-800  py-1 px-2 text-lg "  ><a target="_blank" href="https://github.com/mate-desktop/mate-terminal">Mate</a></button>
+          <button  className="rounded-xl bg-neutral-700 text-neutral-100 hover:text-neutral-200 hover:border-e-neutral-800  py-1 px-2 text-lg "  ><a target="_blank" href="https://mintty.github.io/">Mintty</a></button>
+          <button  className="rounded-xl bg-neutral-700 text-neutral-100 hover:text-neutral-200 hover:border-e-neutral-800  py-1 px-2 text-lg "  ><a target="_blank" href="https://github.com/elementary/terminal">Pantheon / Elementary</a></button>
+          <button  className="rounded-xl bg-neutral-700 text-neutral-100 hover:text-neutral-200 hover:border-e-neutral-800  py-1 px-2 text-lg "  ><a target="_blank" href="https://gnunn1.github.io/tilix-web/">Tilix</a></button>
+          <button  className="rounded-xl bg-neutral-700 text-neutral-100 hover:text-neutral-200 hover:border-e-neutral-800  py-1 px-2 text-lg "  ><a target="_blank" href="https://docs.xfce.org/apps/terminal/start">XFCE4</a></button>
+          <button  className="rounded-xl bg-neutral-700 text-neutral-100 hover:text-neutral-200 hover:border-e-neutral-800  py-1 px-2 text-lg "  ><a target="_blank" href="https://github.com/gnome-terminator/terminator">Terminator</a></button>
+        </ul>
+
+      </section>
+      <section  className="mt-8 w-2/3">
+        <h2 className="mb-8 font-bold text-4xl" >Instal (Linux & macOS)</h2>
+      
+
+        <code className="block w-fit  bg-stone-300 text-stone-950 rounded-lg p-4"><span className="text-stone-500">$</span> bash -c "$(wget -qO- https://git.io/vQgMr)"</code>
+        <span className="block text-xl my-2"> 
+          or
+        </span>
+                 
+        <code className="block w-fit bg-stone-300 text-stone-950 rounded-lg p-4"><span className="text-stone-500">$</span> bash -c "$(curl -sLo- https://git.io/vQgMr)"</code>
+
+      </section>
+      <section  className="w-11/12 mt-8">
+        <div className="flex flex-row gap-2 justify-center place-items-center">
+          <button className="py-3  w-56 text-2xl font-bold rounded-xl bg-amber-700 hover:bg-amber-900 active:bg-amber-950 text-amber-50 " onClick={() => setFilteredBackgrounds(['light','dark'])}>All</button>
+          <button className="py-3  w-56 text-2xl font-bold rounded-xl bg-amber-700 hover:bg-amber-900 active:bg-amber-950 text-amber-50 " onClick={() => setFilteredBackgrounds(['light'])}>Light Themes</button>
+          <button className="py-3  w-56 text-2xl font-bold rounded-xl bg-amber-700 hover:bg-amber-900 active:bg-amber-950 text-amber-50 " onClick={() => setFilteredBackgrounds(['dark'])} >Dark Themes</button>
+        </div>
+        <ul className="mt-8 grid gap-4 grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 ">
+          {themes.map(theme => <li key={theme.name}>
+            <Terminal {...theme} />
+            </li>)}
         </ul>
       </section>
-      <section>
-        <h2>Instal (Linux & macOS)</h2>
-        <h3>Wget</h3>
-        <code>bash -c "$(wget -qO- https://git.io/vQgMr)"</code>
-        <h3>Curl</h3>
-        <code>bash -c "$(curl -sLo- https://git.io/vQgMr)"</code>
-      </section>
-      <section>
-        <div>
-          <button>All</button>
-          <button>Light</button>
-          <button>Dark</button>
-        </div>
-        <div>
-          {themes.map(theme => <Terminal {...theme}></Terminal>)}
-        </div>
-      </section>
-    </body>
+    </main>
   );
 }
 
